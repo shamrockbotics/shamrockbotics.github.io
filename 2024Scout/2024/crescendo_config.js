c632c85a9749cafbbeebcddff477bcd5f8c6cbc3
SHAM-ROCK-BOTICS <shamrockbotics@gmail.com>
2024-03-13 15:04:28 -0400

    updated event code to columbus event

--- 2024Scout/2024/crescendo_config.js
+++ 2024Scout/2024/crescendo_config.js
@@ -8,7 +8,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024inmis", 
+      "defaultValue": "2024incol", 
       "required": "true"
     },
     { "name": "Match Level",
