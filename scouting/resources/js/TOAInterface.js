6c21498765f8d6521eb0d460a4ea3b08a44294b5
Matt Veldman <mattveldman@gmail.com>
2024-12-18 23:40:30 +0000

    Add TOA interface

---
+++ scouting/resources/js/TOAInterface.js
@@ -0,0 +1,47 @@
+// TBAInterface funcitons to pull data from TheBlueAlliance.com
+var teams = null;
+var schedule = null;
+var authKey = "T5BIrNMrmhp22ylI1QmXsbz0G5lyv3tR9gN9oLaNCE4";
+/**
+ * Get list of teams in event
+ *
+ * @param {eventCode} eventCode the event code (i.e. 2020caln) to pull the team list
+ */
+function getTeams(eventCode) {
+	if (authKey) {
+		var xmlhttp = new XMLHttpRequest();
+		var url = "https://www.theorangealliance.org/api/event/" + eventCode + "/teams";
+		xmlhttp.open("GET", url, true);
+		xmlhttp.setRequestHeader("X-TOA-Key", authKey);
+		xmlhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				var response = this.responseText;
+				teams = JSON.parse(response);
+			}
+		};
+		// Send request
+		xmlhttp.send();
+	}
+}
+
+/**
+ * Get schefule for event
+ *
+ * @param {eventCode} eventCode the event code (i.e. 2020caln) to pull the team list
+ */
+function getSchedule(eventCode) {
+	if (authKey) {
+		var xmlhttp = new XMLHttpRequest();
+		var url = "https://www.theorangealliance.org/api/event/" + eventCode + "/matches";
+		xmlhttp.open("GET", url, true);
+		xmlhttp.setRequestHeader("X-TOA-Key", authKey);
+		xmlhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				var response = this.responseText;
+				schedule = JSON.parse(response);
+			}
+		};
+		// Send request
+		xmlhttp.send();
+	}
+}
