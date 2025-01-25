a590df63e4e23b7002c95846c01c7ef8128d5aad
Matt Veldman <mattveldman@gmail.com>
2025-01-25 16:43:39 -0500

    Update default event

--- scouting/2025ftc/into_the_deep_config.js
+++ scouting/2025ftc/into_the_deep_config.js
@@ -10,7 +10,7 @@ var config_data = `
             "name": "Event",
             "code": "e",
             "type": "event",
-            "defaultValue": "2425-IN-INQ",
+            "defaultValue": "2425-IN-WLQ2",
             "required": "true"
         },
         {
